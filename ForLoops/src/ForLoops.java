b0ec886a3df4597a28ffe119c902ad8d372ec23d
dunham.perry <dunham.perry@MU-C1-05.mullen.pvt.k12.co.us>
2018-08-27 08:45:56 -0600

    initial commit

---
+++ ForLoops/src/ForLoops.java
@@ -0,0 +1,61 @@
+
+public class ForLoops
+	{
+
+		public static void main(String[] args)
+			{
+				//showGeniusOne();
+				//System.out.println();
+				//showGeniusTwo();
+				//System.out.println();
+				//showGeniusThree();
+				//System.out.println();
+				//showGeniusFour();
+				//System.out.println();
+				showGeniusFive();
+				System.out.println();
+				showGeniusSix();
+				System.out.println();
+				showGeniusSeven();
+				System.out.println();
+				showGeniusEight();
+			}
+		public static void showGeniusOne(){
+			for (int i = 1; i<6; i++){
+				System.out.println(i);
+			}
+		}
+		public static void showGeniusTwo(){
+			for (int i = 2; i<11; i=i+2){
+				System.out.println(i + ") I Love Mullen");
+			}
+		}
+		public static void showGeniusThree(){
+			for (int i = 1; i<6; i++){
+				System.out.print(i);
+			}
+		}
+		public static void showGeniusFour(){
+			for (int i = 10; i>0; i--){
+				System.out.println(i);
+			}
+			System.out.println("LIFTOFF!");
+		}
+		public static void showGeniusFive(){
+			int counter =0;
+			for (int i = 5; i < 101; i=i+5){
+				counter = counter + i;
+			}
+			System.out.println(counter);
+		}
+		public static void showGeniusSix(){
+			
+		}
+		public static void showGeniusSeven(){
+			
+		}
+		public static void showGeniusEight(){
+			
+		}
+
+	}
